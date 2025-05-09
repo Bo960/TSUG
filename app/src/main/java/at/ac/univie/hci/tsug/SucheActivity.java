b87142c147180105b013a351a2723762d390e427
seriousmichael <feldbahn@outlook.at>
2025-05-09 17:09:10 +0200

    activity_suche erstellt arrays erstellt SucheActivity erstellt

---
+++ app/src/main/java/at/ac/univie/hci/tsug/SucheActivity.java
@@ -0,0 +1,20 @@
+package at.ac.univie.hci.tsug;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SucheActivity extends AppCompatActivity {
+
+}
